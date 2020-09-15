991b1dd91c8fd2a835eb49c65363a7e3ef4ff20b
ChrisFrench123 <chrisfrenchtoaster@gmail.com>
2020-09-15 16:36:21 +0100

    This is an upload of the final finished program

---
+++ Generator.py
@@ -0,0 +1,50 @@
+# Chris French 24.04.2020 - Epicycloid Generator using Python Turtle
+
+# Turtle Setup
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("light Blue")
+wn.title("Turtle")
+t = turtle.Turtle()
+t.hideturtle()
+t._tracer(0)
+
+# variable inputs
+multiplier = int(input("what do you want the multiplier to be?"))
+steps = int(input("How many steps do you want?"))
+coordinates = {}
+
+def PLotNumbers():
+    global coordinates
+    rotate = 360/steps
+    t.penup()
+    t.goto(0, 0)
+    t.setheading(0)
+    for i in range (steps):
+        t.goto(0,0)
+        t.forward(200)
+        t.dot()
+        CurrentPos = t.pos()
+        coordinates[i] = (CurrentPos)
+        t.left(rotate)
+
+
+def ConnectNumbers():
+    global coordinates
+    for i in range(len(coordinates)):
+        firstCoord = coordinates[i]
+        # find the number to connect it to
+        multiplied = int(i)*multiplier
+        while multiplied >= steps: # if it too big, loop over
+            multiplied = multiplied - steps
+        # get the coord of the second number
+        secondCoord = coordinates[multiplied]
+        # draw the lines
+        t.penup()
+        t.goto(firstCoord)
+        t.pendown()
+        t.goto(secondCoord)
+
+PLotNumbers()
+ConnectNumbers()
+turtle.done()
