29609582848ab72f87711f0821f52eb98399a3e2
Matt French <matthewfrench019@gmail.com>
2020-11-13 09:55:07 +0000

    Proposed Change

--- Generator.py
+++ Generator.py
@@ -1,50 +1,59 @@
 # Chris French 24.04.2020 - Epicycloid Generator using Python Turtle
-#test
-# Turtle Setup
 import turtle
-wn = turtle.Screen()
-wn.bgcolor("light Blue")
-wn.title("Turtle")
-t = turtle.Turtle()
-t.hideturtle()
-t._tracer(0)
 
-# variable inputs
-multiplier = int(input("what do you want the multiplier to be?"))
-steps = int(input("How many steps do you want?"))
-coordinates = {}
 
-def PLotNumbers():
-    global coordinates
-    rotate = 360/steps
-    t.penup()
-    t.goto(0, 0)
-    t.setheading(0)
-    for i in range (steps):
-        t.goto(0,0)
-        t.forward(200)
-        t.dot()
-        CurrentPos = t.pos()
-        coordinates[i] = (CurrentPos)
-        t.left(rotate)
+class EpicycloidGenerator:
+    def __init__(self):
+        self.coords = {}
 
-# I have changed some stuff for the Drawing Branch
-def ConnectNumbers():
-    global coordinates
-    for i in range(len(coordinates)):
-        firstCoord = coordinates[i]
-        # find the number to connect it to
-        multiplied = int(i)*multiplier
-        while multiplied >= steps: # if it too big, loop over
-            multiplied = multiplied - steps
-        # get the coord of the second number
-        secondCoord = coordinates[multiplied]
-        # draw the lines
-        t.penup()
-        t.goto(firstCoord)
-        t.pendown()
-        t.goto(secondCoord)
+        # User Inputs
+        self.multiplier = int(input("What do you want the multiplier to be? "))
+        self.steps = int(input("How many steps do you want? "))
 
-PLotNumbers()
-ConnectNumbers()
-turtle.done()
+        ### Turtle Setup ###
+        self.wn = turtle.Screen()       # Create Screen
+        self.wn.bgcolor("light Blue")   # Set the colour
+        self.wn.title("Turtle")         # Set name of window
+
+        self.t = turtle.Turtle()        # Create Turtle
+        self.t.hideturtle()             # Remove the Turtle body
+        self.t._tracer(0)                # Remove the tracing
+
+    def plot_numbers(self):
+        rotate = 360/self.steps
+        self.t.penup()
+        self.t.goto(0, 0)
+        self.t.setheading(0)
+        for i in range(self.steps):
+            self.t.goto(0, 0)
+            self.t.forward(200)
+            self.t.dot()
+            current_pos = self.t.pos()
+            self.coords[i] = current_pos
+            self.t.left(rotate)
+
+    # I have changed some stuff for the Drawing Branch
+    def connect_numbers(self):
+        for i in range(len(self.coords)):
+            first_coord = self.coords[i]
+            # find the number to connect it to
+            multiplied = int(i)*self.multiplier
+            while multiplied >= self.steps: # if it too big, loop over
+                multiplied = multiplied - self.steps
+            # get the coord of the second number
+            second_coord = self.coords[multiplied]
+            # draw the lines
+            self.t.penup()
+            self.t.goto(first_coord)
+            self.t.pendown()
+            self.t.goto(second_coord)
+
+    def draw(self):
+        self.plot_numbers()
+        self.connect_numbers()
+        turtle.done()
+
+
+if __name__ == '__main__':
+    drawer = EpicycloidGenerator()
+    drawer.draw()
